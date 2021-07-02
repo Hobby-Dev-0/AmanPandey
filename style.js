93823d0e671684aac736dff80f23c947cc999649
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-02 12:34:30 +0530

    D

---
+++ style.js
@@ -0,0 +1,12 @@
+function changecolor1(){
+  document.getElementById("htmlDiv").style.color="red";
+}
+function changecolor2(){
+  document.getElementById("htmlDiv").style.color="white";
+}
+function changecolor3(){
+  document.getElementById("cssDiv").style.color="black";
+}
+function changecolor4(){
+  document.getElementById("cssDiv").style.color="white";
+}
