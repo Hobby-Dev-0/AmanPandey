855431d86423179d5ac53c0853eb772d9087a939
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2021-10-02 10:19:59 +0530

    Why wont to change colors hehe

--- style.js
+++ style.js
@@ -5,8 +5,8 @@ function changecolor2(){
   document.getElementById("htmlDiv").style.color="white";
 }
 function changecolor3(){
-  document.getElementById("cssDiv").style.color="black";
+  document.getElementById("cssDiv").style.color="blue";
 }
 function changecolor4(){
-  document.getElementById("cssDiv").style.color="white";
+  document.getElementById("cssDiv").style.color="red";
 }
